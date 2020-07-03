450d2747ed030422f09e259ed26313ac532aca3b
Akhilesh Anand <akhilesh.anand@sjsu.edu>
2020-07-03 09:45:49 -0700

    first commit

---
+++ src/main/java/com/akhianand/springrolejwt/SpringRoleJwtApplication.java
@@ -0,0 +1,13 @@
+package com.akhianand.springrolejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRoleJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRoleJwtApplication.class, args);
+	}
+
+}
